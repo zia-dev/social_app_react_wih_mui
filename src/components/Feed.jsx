f3f20a46efa182d7cd92d4e8160249c38ca4a70c
Zia Ullah <58667414+zia-dev@users.noreply.github.com>
2022-10-15 00:23:26 +0500

    setting the envirnoment

---
+++ src/components/Feed.jsx
@@ -0,0 +1,12 @@
+import { Box } from '@mui/material'
+import React from 'react'
+
+const Feed = () => {
+  return (
+    <Box bgcolor={'lightcoral'}  flex={4}>
+      Feed
+    </Box>
+  )
+}
+
+export default Feed
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+import { Abc, Mail, Notifications } from "@mui/icons-material";
+import {
+  AppBar,
+  Avatar,
+  Badge,
+  Box,
+  InputBase,
+  Menu,
+  MenuItem,
+  styled,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+
+const StyledToolbar = styled(Toolbar)({
+  display: "flex",
+  justifyContent: "space-between",
+});
+
+const Search = styled("div")(({ theme }) => ({
+  backgroundColor: "white",
+  padding: "0 10px",
+  borderRadius: theme.shape.borderRadius,
+  width: "40%",
+}));
+
+const Icons = styled("Box")(({ theme }) => ({
+  display: "none",
+  alignItems: "center",
+  gap: "20px",
+  [theme.breakpoints.up("sm")]: {
+    display: "flex",
+  },
+}));
+const Navbar = () => {
+  return (
+    <AppBar position="sticky">
+      <StyledToolbar>
+        <Typography variant="h6" sx={{ display: { xs: "none", sm: "block" } }}>
+          ZIA DEV
+        </Typography>
+        <Abc sx={{ display: { xs: "block", sm: "none" } }} />
+        <Search>
+          <InputBase placeholder="search" />
+        </Search>
+        <Icons>
+          <Badge badgeContent={4} color="error">
+            <Mail />
+          </Badge>
+          <Badge badgeContent={2} color="error">
+            <Notifications />
+          </Badge>
+        </Icons>
+      </StyledToolbar>
+    </AppBar>
+  );
+};
+
+export default Navbar;
